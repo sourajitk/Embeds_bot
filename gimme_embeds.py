3117e5e0ca122020a819ebf30347d20f61579925
Filippo Roggi <zzkw35@gmail.com>
2022-08-12 17:43:33 +0200

    Handle hyperlinks
    And create the `edit_text` method.
    
    Add and update comments
    
    Run black code formatter

--- gimme_embeds.py
+++ gimme_embeds.py
@@ -28,28 +28,44 @@ def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hello")
 
 
+def edit_text(text):
+    """
+    This function edits the text of the message, by replacing some
+    website URLs with their vx counterparts that support embeds.
+    """
+    # Declare our list of to-be-replaced and new URLs.
+    list_of_urls = ["twitter.com/", "tiktok.com/"]
+
+    # Setup conditionals.
+    # For Twitter
+    if list_of_urls[0] in text:
+        new_url = text.replace("twitter", "vxtwitter")
+    # For TikTok
+    elif list_of_urls[1] in text:
+        new_url = text.replace("tiktok", "vxtiktok")
+    return new_url
+
+
 def text_handler(update, context):
     """
     The primary handler that does all the replacement action through
     the API.
     """
-    # Declare our list of to-be-replaced and new URLs.
-    list_of_urls = ["twitter.com/", "tiktok.com/"]
 
     # Use variables derived from custom API methods.
     message = update.message.text
     reply = update.message.reply_text
 
-    # Setup conditionals for TikTok and Twitter.
-    # For Twitter
-    if list_of_urls[0] in message:
-        new_url = message.replace("twitter", "vxtwitter")
-        reply(new_url)
-
-    # For TikTok
-    elif list_of_urls[1] in message:
-        new_url = message.replace("tiktok", "vxtiktok")
-        reply(new_url)
+    """
+    Check if the text contains an hyperlink.
+    If it does, extract the URL and pass it to the edit_text function.
+    If it doesn't, send the entire text to edit_text.
+    """
+    if update.message.entities[0].url is None:
+        reply(edit_text(message))
+    else:
+        url = update.message.entities[0].url
+        reply(edit_text(url))
 
 
 start_handler = CommandHandler("start", start)
