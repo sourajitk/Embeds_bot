0409a8fe2eff881239da0ffebf7a9969f3efa569
Chris Thurber <skiman6010@gmail.com>
2022-08-13 13:07:28 -0400

    Convert our bot to a module

---
+++ src/app.py
@@ -0,0 +1,34 @@
+from gimme_embeds import GimmeEmbeds
+import os
+import logging
+from dotenv import load_dotenv
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+)
+
+load_dotenv()
+ge = GimmeEmbeds()
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
+)
+
+# Use variables derived from custom API methods.
+updater = Updater(token=os.getenv("BOT_TOKEN"), use_context=True)
+dispatcher = updater.dispatcher
+
+
+def main():
+    start_handler = CommandHandler("start", ge.start)
+    dispatcher.add_handler(start_handler)
+    message_handler = MessageHandler(Filters.text, ge.text_handler)
+    dispatcher.add_handler(message_handler)
+
+    updater.start_polling()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/gimme_embeds.py
@@ -0,0 +1,67 @@
+"""
+Embeds here we come!
+"""
+import re
+
+
+class GimmeEmbeds:
+    def start(self, update, context):
+        """
+        This function sets up the /start action to let the user know the
+        bot is alive and ready to go.
+        """
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Hello")
+
+    def edit_text(self, text):
+        """
+        This function edits the text of the message, by replacing some
+        website URLs with their vx counterparts that support embeds.
+        """
+
+        # Setup conditionals.
+        # For Twitter
+        if re.search(
+            "(?P<url>twitter.com[^\s]+)", text, re.IGNORECASE
+        ) and not re.search("(?P<url>xtwitter.com[^\s]+)", text, re.IGNORECASE):
+            # Isolate the Twitter URL.
+            twitter_url = str(
+                re.search("(?P<url>twitter.com[^\s]+)", text, re.IGNORECASE).group(
+                    "url"
+                )
+            )
+            insensitive_twitter = re.compile(re.escape("twitter.com"), re.IGNORECASE)
+            new_url = insensitive_twitter.sub("vxtwitter.com", twitter_url)
+            new_url = new_url.split("?")[0]  # Remove trackers
+
+        # For TikTok
+        elif re.search(
+            "(?P<url>tiktok.com[^\s]+)", text, re.IGNORECASE
+        ) and not re.search("(?P<url>xtiktok.com[^\s]+)", text, re.IGNORECASE):
+            # Isolate the tiktok URL.
+            tiktok_url = str(
+                re.search("(?P<url>tiktok[^\s]+)", text, re.IGNORECASE).group("url")
+            )
+            insensitive_tiktok = re.compile(re.escape("tiktok.com"), re.IGNORECASE)
+            new_url = insensitive_tiktok.sub("vxtiktok.com", tiktok_url)
+        return new_url
+
+    def text_handler(self, update, context):
+        """
+        The primary handler that does all the replacement action through
+        the API.
+        """
+
+        # Use variables derived from custom API methods.
+        message = update.message.text
+        reply = update.message.reply_text
+
+        """
+        Check if the text contains an hyperlink.
+        If it does, extract the URL and pass it to the edit_text function.
+        If it doesn't, send the entire text to edit_text.
+        """
+        if update.message.entities[0].url is None:
+            reply(self.edit_text(message))
+        else:
+            url = update.message.entities[0].url
+            reply(self.edit_text(url))
