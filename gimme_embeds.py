6055fe86a697b301b91330d4718c3ef3b8cd6670
Filippo Roggi <zzkw35@gmail.com>
2022-08-14 12:18:03 +0200

    Analyse only the texts containing URLs or hyperlinks
    Ignore any other kind of content.

--- gimme_embeds.py
+++ gimme_embeds.py
@@ -90,7 +90,9 @@ def text_handler(update, context: CallbackContext):
 
 start_handler = CommandHandler("start", start)
 dispatcher.add_handler(start_handler)
-message_handler = MessageHandler(Filters.text, text_handler)
+message_handler = MessageHandler(
+    (Filters.entity("url") | Filters.entity("text_link")), text_handler
+)
 dispatcher.add_handler(message_handler)
 
 updater.start_polling()
