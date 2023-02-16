24982d095a5fb546c3408e79c7b0e132084db3b5
Filippo Roggi <zzkw35@gmail.com>
2023-02-17 00:00:31 +0100

    Fix unit tests and run black formatter

--- tests/test_embeds.py
+++ tests/test_embeds.py
@@ -12,6 +12,7 @@ mocked_context = Mock()
 
 ge = GimmeEmbeds()
 
+
 def test_start():
     """Test the start function."""
     ge.start(mocked_update, mocked_context)
@@ -23,31 +24,52 @@ def test_source():
     ge.source(mocked_update, mocked_context)
     mocked_context.bot.send_message.assert_called()
 
+
 def test_edit_text_twitter():
     """Test the edit_text function for Twitter."""
-    assert ge.edit_text("twitter.com/prince/status/1380000000000000000") == "vxtwitter.com/prince/status/1380000000000000000" # pylint: disable=line-too-long
-    assert ge.edit_text("twitter.com/i/spaces/1380000000000000000") is None
-    assert ge.edit_text("twitter.com/i/events/1380000000000000000") is None
-    assert ge.edit_text("vxtwitter.com/prince/status/1380000000000000000") is None
+    assert (
+        ge.edit_text("twitter.com/prince/status/1380000000000000000", 000000000)
+        == "vxtwitter.com/prince/status/1380000000000000000"
+    )  # pylint: disable=line-too-long
+    assert ge.edit_text("twitter.com/i/spaces/1380000000000000000", 000000000) is None
+    assert ge.edit_text("twitter.com/i/events/1380000000000000000", 000000000) is None
+    assert (
+        ge.edit_text("vxtwitter.com/prince/status/1380000000000000000", 000000000)
+        is None
+    )
+
 
 def test_edit_text_tiktok():
     """Test the edit_text function for TikTok."""
-    assert ge.edit_text("tiktok.com/@prince/video/6900000000000000000") == "vxtiktok.com/@prince/video/6900000000000000000" # pylint: disable=line-too-long
-    assert ge.edit_text("vxtiktok.com/@prince/video/6900000000000000000") is None
+    assert (
+        ge.edit_text("tiktok.com/@prince/video/6900000000000000000", 000000000)
+        == "vxtiktok.com/@prince/video/6900000000000000000"
+    )  # pylint: disable=line-too-long
+    assert (
+        ge.edit_text("vxtiktok.com/@prince/video/6900000000000000000", 000000000)
+        is None
+    )
+
 
 def test_edit_text_instagram():
     """Test the edit_text function for Instagram."""
-    assert ge.edit_text("instagram.com/p/CO0000000000000000000/") == "ddinstagram.com/p/CO0000000000000000000/" # pylint: disable=line-too-long
-    assert ge.edit_text("ddinstagram.com/p/CO0000000000000000000/") is None
+    assert (
+        ge.edit_text("instagram.com/p/CO0000000000000000000/", 000000000)
+        == "ddinstagram.com/p/CO0000000000000000000/"
+    )  # pylint: disable=line-too-long
+    assert ge.edit_text("ddinstagram.com/p/CO0000000000000000000/", 000000000) is None
+
 
 def test_edit_text_invalid():
     """Test the edit_text function for invalid URLs."""
-    assert ge.edit_text("invalid_url") is None
+    assert ge.edit_text("invalid_url", 000000000) is None
+
 
 def test_edit_text_empty():
     """Test the edit_text function for empty string."""
-    assert ge.edit_text("") is None
+    assert ge.edit_text("", 000000000) is None
+
 
 def test_edit_text_no_url():
     """Test the edit_text function for no URL."""
-    assert ge.edit_text("test message") is None
+    assert ge.edit_text("test message", 000000000) is None
